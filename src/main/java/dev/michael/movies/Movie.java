e7ebf2f50507b7200134ca9d5c240413c4308f24
michaelherman <michaelbherman@gmail.com>
2023-06-13 00:39:22 -0600

    Test for github

--- src/main/java/dev/michael/movies/Movie.java
+++ src/main/java/dev/michael/movies/Movie.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
+//making change//
 
 @Document(collection = "movies") //This annotation marks a class as being a domain object that we want to persist to the database: It also allows us to choose the name of the collection we want to use: (collection = "name")
 
